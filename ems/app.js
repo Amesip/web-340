a6627560d3e65254f49a8d6eb33d81d91c970415
Mary Barrett <mkbarrett.1201@gmail.com>
2018-04-15 15:43:41 -0500

    Adding Week 8

--- ems/app.js
+++ ems/app.js
@@ -2,7 +2,27 @@
 
 var mongoose = require("mongoose");
 
+var helmet = require("helmet");
+
+var bodyParser = require("body-parser");
+
+var cookieParser = require("cookie-parser");
+
+var csrf = require("csurf");
+
+// setup csrf protection
+
+var csrfProtection = csrf({cookie: true});
+
 var employee = require("./models/employee");
+
+var express = require("express");
+
+var http = require("http");
+
+var path = require("path");
+
+var logger = require("morgan");
 
 // mLab connection
 
@@ -26,20 +46,35 @@ db.once("open", function() {
 
 });
 
-var mongoose = require("mongoose");
-
 var Schema = mongoose.Schema;
 
-
-var express = require("express");
-
-var http = require("http");
-
-var path = require("path");
-
-var logger = require("morgan");
-
 var app = express();
+
+app.use(helmet.xssFilter());
+
+app.use(bodyParser.urlencoded({
+
+    extended: true
+
+}));
+
+app.use(cookieParser());
+
+app.use(csrfProtection);
+
+app.use(function(request, response, next) {
+
+    var token = request.csrfToken();
+
+    response.cookie('XSRF-TOKEN', token);
+
+    response.locals.csrfToken = token;
+
+    next();
+
+});
+
+app.use(bodyParser.json());
 
 app.set("views", path.resolve(__dirname, "views"));
 
@@ -51,14 +86,6 @@ app.use(logger("short"));
 
 // model
 
-var Employee = new employee.Employee({
-
-    firstName: "Mary",
-    lastName: "Barrett"
-
-});
-
-
 app.get("/", function (request, response) {
 
     response.render("index", {
@@ -68,6 +95,73 @@ app.get("/", function (request, response) {
     });
 
 });
+
+// routing
+
+app.get("/new", function(request, response) {
+
+    response.render("new", {
+
+        message: "New Employee Entry Page"
+
+    });
+
+});
+
+app.get("/list", function(request, response) {
+        employee.Employee.find({}, function(error, employees) {
+           if (error) throw error;
+           response.render("list", {
+               title: "Employee List",
+               employees: employees
+           });
+        });
+    });
+
+app.post("/process", function(request, response) {
+
+    // console.log(request.body.txtName);
+ 
+    if (!request.body.txtFName && !request.body.txtLName) {
+ 
+        response.status(400).send("Entries must have a name");
+ 
+        return;
+ 
+    }
+ 
+    // get the request's form data
+ 
+    var firstName = request.body.txtFName;
+ 
+    console.log(firstName);
+
+    var lastName = request.body.txtLName;
+
+    console.log(lastName);
+ 
+    // create a fruit model
+ 
+    var newEmployee = new employee.Employee({
+ 
+        firstName: firstName,
+        lastName: lastName
+ 
+    });
+ 
+    // save
+ 
+    newEmployee.save(function (error) {
+ 
+        if (error) throw error;
+ 
+        console.log(firstName + lastName + " saved successfully!");
+ 
+    });
+ 
+    response.redirect("/");
+ 
+ });
 
 http.createServer(app).listen(8080, function() {
 
